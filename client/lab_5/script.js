2512b49bce1c444be3317a8a1ac13c15645084d3
EthanMeyer41 <emeyer15@terpmail.umd.edu>
2023-03-24 02:50:30 -0400

    style

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -5,18 +5,26 @@
 
 /* A quick filter that will return something based on a matching input */
 function filterList(list, query) {
+
+  return list.filter( (item) => {
+    const lowercaseName = item.name.toLowerCase();
+    const lowercaseQuery = query.toLowerCase();
+    return lowercaseName.includes(lowercaseQuery);
+  })
   /*
     Using the .filter array method, 
     return a list that is filtered by comparing the item name in lower case
     to the query in lower case
-
     Ask the TAs if you need help with this
   */
+
+  
 }
 
 async function mainEvent() { // the async keyword means we can make API requests
   const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
-  // Add a querySelector that targets your filter button here
+  const filterButton = document.querySelector('.filterButton');// Add a querySelector that targets your filter button here
+  const restaurantList = document.querySelector('.restaurantList');
 
   let currentList = []; // this is "scoped" to the main event function
   
@@ -54,15 +62,24 @@ async function mainEvent() { // the async keyword means we can make API requests
     console.table(currentList); 
   });
 
+  filterButton.addEventListener('click', (event) => {
+    console.log("Clicked Filter Button");
+
+    const formData = new FormData(mainForm);
+    const formProps = Object.fromEntries(formData);
+
+    console.log(formProps);
+    const newList = filterList(currentList, formProps.resto)
+    console.log(newList);
+    
+  })
 
   /*
     Now that you HAVE a list loaded, write an event listener set to your filter button
     it should use the 'new FormData(target-form)' method to read the contents of your main form
     and the Object.fromEntries() method to convert that data to an object we can work with
-
     When you have the contents of the form, use the placeholder at line 7
     to write a list filter
-
     Fire it here and filter for the word "pizza"
     you should get approximately 46 results
   */
